b04a03f5c50e84c7bb9fd20b44458fb82c6616f0
Shqipdon Mehmeti <keds30671@KEDS.kv>
2023-10-10 18:51:49 +0200

    multiple functions added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,19 @@
-
+import java.util.Scanner
 
 fun main(args: Array<String>) {
-    printoTeDhenat("Shqipdon","Mehmeti","01.01.01")
-    println(sumThreeNumbers(1,2,3))
-    println(nameOrSurname(true))
-    readAndWriteData()
-    printData()
+//    printoTeDhenat("Shqipdon","Mehmeti","01.01.01")
+//    println(sumThreeNumbers(1,2,3))
+//    println(nameOrSurname(true))
+//    readAndWriteData()
+//    printData()
+//    createScanner()
+//    val scanner = Scanner(System.`in`)
+//    println("Write word : ")
+//    val word = scanner.nextLine()
+//    println("Write repeat times : ")
+//    val numberOfRepeat = scanner.nextInt()
+//    printAndRepeat(word,numberOfRepeat)
+    print(convertData(100))
 }
 
 fun printoTeDhenat(emri : String, mbiemri : String , dataELindjes : String) {
@@ -41,6 +49,28 @@ fun printData() {
     println("Please write your height : ")
     val height : Float = readln().toFloat()
     println("Birthdate : $birthDate , age : $age , height : $height")
+}
+
+fun createScanner() {
+    val scanner = Scanner(System.`in`)
+    println("Write today date : ")
+    val todayDate = scanner.nextLine()
+    println("Write tomorrow date : ")
+    val tomorrowDate = scanner.nextLine()
+//    println("Today date : " + todayDate + ", " + " Tomorrow date : " + tomorrowDate )
+    println("Today date : $todayDate , Tomorrow date : $tomorrowDate")
+}
+
+fun printAndRepeat(word : String , numberOfRepeat : Int) {
+    println(word.plus("\n").repeat(numberOfRepeat))
+}
+
+
+fun convertData(firstType : Byte) : String {
+    val secondType : Short = firstType.toShort()
+    val thirdType : Int = secondType.toInt()
+    val fourthType : Long = thirdType.toLong()
+    return fourthType.toString()
 }
 
 
