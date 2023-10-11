407f109520ece003e664b27efca5bd4564e7bf92
Shqipdon Mehmeti <keds30671@KEDS.kv>
2023-10-11 20:55:47 +0200

    more functions added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,8 +15,21 @@ fun main(args: Array<String>) {
 //    val numberOfRepeat = scanner.nextInt()
 //    printAndRepeat(word,numberOfRepeat)
 //    print(convertData(100))
-    println("Shkruaj numrin : ")
-    print(isOdd(readln().toInt()))
+//    println("Shkruaj numrin : ")
+//    print(isOdd(readln().toInt()))
+
+//    println(concatTwoString("Hello","World")) // default argument
+//    println(concatTwoString(a = "Hello", b = "World")) // named arguments in order
+//    println(concatTwoString(b = "Hello", a = "World")) // named arguments unordered
+//    stringToNumber()
+//    identifyTraits()
+//    printOddOrEven(false)
+//    println(subTwoNumbers(10,5))
+
+//    sumThreeNumbersShortWay(1,2,3) // default arguments
+//    sumThreeNumbersShortWay(a = 3,c = 5 , b = 4) // named arguments
+//    stringToNumber2()
+
 }
 
 fun printoTeDhenat(emri : String, mbiemri : String , dataELindjes : String) {
@@ -30,6 +43,11 @@ fun sumThreeNumbers(a : Int , b : Int , c : Int) : Int {
 //    return sum
     return a + b + c
 }
+
+fun sumThreeNumbersShortWay(a : Int , b : Int , c : Int) = a + b + c
+
+fun subTwoNumbers2(a : Int , b : Int) = a - b
+
 
 fun nameOrSurname(isName : Boolean) : String {
     if (isName) return "Emri"
@@ -76,12 +94,24 @@ fun convertData(firstType : Byte) : String {
     return fourthType.toString()
 }
 
+
+
 fun calculator(operator : String , a : Int , b : Int) : Int {
-    if (operator == "+") return a + b
-    else if (operator == "-") return a - b
-    else if (operator == "*") return a * b
-    else if (operator == "/") return a / b
+    if (operator == "+" || operator == "plus") return a + b
+    else if (operator == "-" || operator == "minus") return a - b
+    else if (operator == "*" || operator == "times") return a * b
+    else if (operator == "/" || operator == "divide") return a / b
     else return 0
+}
+
+fun calculatorWithWhenExpression(operator : String , a : Int , b : Int) : Int {
+    return when (operator) {
+        "+", "plus" -> a + b
+        "-", "minus" -> a - b
+        "*", "times" -> a * b
+        "/", "divide" -> a / b
+        else -> 0
+    }
 }
 
 fun isOdd(number : Int) : Boolean {
@@ -89,6 +119,68 @@ fun isOdd(number : Int) : Boolean {
 //    else return false
     return number % 2 != 0
 }
+
+
+//fun sumTwoNumbers(a : Int , b : Int) : Int {
+//    return a + b
+//}
+
+fun sumTwoNumbers(a : Int , b : Int) = a + b
+
+fun subTwoNumbers(a : Int , b : Int) = a - b
+
+fun concatTwoString(a : String , b : String) = a.plus(b)
+
+//fun isVowel(character : Char) : Boolean {
+//    if (character == 'a' || character == 'e' || character == 'i'
+//        || character == 'o' || character == 'u' || character == 'y') return true
+//    else return false
+//}
+
+fun isVowel(character : Char) =
+    (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'y')
+
+
+fun stringToNumber() {
+    println("Shkruani numrin prej 1 deri ne 5 si string : ")
+    val word = readLine()?.lowercase()
+    val result = when(word) {
+        "one" -> 1
+        "two" -> 2
+        "three" -> 3
+        "four" -> 4
+        "five" -> 5
+        else -> 0
+    }
+    println(result)
+}
+
+fun identifyTraits() {
+    println("Write house characteristics : ")
+    val line = readln()
+    val result = when(line) {
+        "gryffindor" -> "bravery"
+        "hufflepuff" -> "loyality"
+        else -> "Not a valid house"
+    }
+    println(result)
+}
+
+fun printFrom25to50() {
+    for (i in 25 until 50) {
+        println(i)
+    }
+}
+
+fun printOddOrEven(isOdd : Boolean) {
+    if (isOdd) {
+        for (i in 1..100 step 2) println(i)
+    } else {
+        for (i in 0..100 step 2) println(i)
+    }
+}
+
+
 
 
 
